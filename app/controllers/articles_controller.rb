4f8c2c5ec120339f8cf66a2d43ab558c5813c344
Li Wang <llwang8@yahoo.com>
2015-10-02 14:50:10 +0000

    first commit for send SMS

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,32 @@
+class ArticlesController < ApplicationController
+  def new
+    
+  end
+  
+  def create
+    phone_num = params[:article]
+
+    account_sid = 'ACa5318e7db84ed99e5a2bbb26f999de8e'
+    auth_token = '176b516a5cf1090f99f24d9afaa47331'
+    @client = Twilio::REST::Client.new account_sid, auth_token
+
+    from = '+16092073156' # Your Twilio number
+    #to = '+16099334843'
+    to = phone_num
+    to = to.gsub("(", "")
+    to = to.gsub(")", "")
+    to = to.gsub("-", "")
+    to = to.gsub("/", "")
+    to = to.gsub("*", "")
+    to = to.gsub("%", "")
+    to = to.gsub(/\s+/, "")
+    to = to.to_i
+
+    @client.account.messages.create(
+    :from => from,
+    :to => to,
+    :body => 'Hey, Monkey party at 6PM. Bring Bananas!'
+    )
+  end
+  
+end
