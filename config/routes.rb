82101d4214149985ee04eed1ee0f0ff3584c22af
Li Wang <llwang8@yahoo.com>
2015-10-07 19:31:02 +0000

    routes again

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :articles
-  root get / 'articles#new'
+  #root get / 'articles#new'
+  root :controller => 'articles', :action => '/new' 
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
