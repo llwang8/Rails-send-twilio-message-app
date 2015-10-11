7cfe8f327f76cf7875eced7e8ced1735cb225f49
Li Wang <llwang8@yahoo.com>
2015-10-11 18:53:19 +0000

    validation in article.rb, msg in new.html.erb, put back @article.save

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,3 @@
 class Article < ActiveRecord::Base
-    #validates :phone_number, presence: true
+    validates :phone_number, presence: true, numericality: { only_integer: true }, length: { is: 10 }
 end
--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,7 @@ module SendApp
   class Application < Rails::Application
     # the new line added for autoload of lib
     config.autoload_paths += %W(#{config.root}/lib)
-    config.assets.initialize_on_precompile = false
+    #config.assets.initialize_on_precompile = false
     
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
